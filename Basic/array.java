7a9184278d9ea33572d974296e55945bb6d42069
MyaPah.githhub.io <myapah7605@gmail.com>
2023-10-31 20:57:36 +0700

    nothing

--- Basic/array.java
+++ Basic/array.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class array {
     public static void main(String[] args) {
             int[] a={1,2,3,4};
+            
             System.out.println(Arrays.toString(a));
 
     }
